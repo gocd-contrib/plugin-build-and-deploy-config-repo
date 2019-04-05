b2ba2022fd8daf4b85d2de507d8bd9a6b0803540
Bhupendrakumar Piprava <bdpiparva@gmail.com>
2019-04-05 12:57:31 +0530

    Added prerelease and release stage for plugins pipeline

--- build.gocd.groovy
+++ build.gocd.groovy
@@ -7,12 +7,20 @@ def allRepos = [
     "github-oauth-authorization-plugin",
     "gocd-groovy-dsl-config-plugin"
   ],
-  "gocd": [
+  "gocd"        : [
     "gocd-file-based-secrets-plugin",
     "gocd-filebased-authentication-plugin",
-    "kubernetes-elastic-agents"
+    "kubernetes-elastic-agents",
+    "docker-registry-artifact-plugin"
   ]
 ]
+
+def releaseCredentials = { org ->
+  return [
+    GITHUB_USER : org,
+    GITHUB_TOKEN: 'foo',
+  ]
+}
 
 GoCD.script {
   pipelines {
@@ -22,18 +30,42 @@ GoCD.script {
           materials {
             git {
               url = "https://git.gocd.io/git/${org}/${repo}"
+              shallowClone = false
             }
           }
-          group = "plugins"
+          group = "gocd" == org ? "supported_plugins" : "plugins"
           stages {
             stage("test") {
               jobs {
                 job("test") {
                   elasticProfileId = "ecs-gocd-dev-build"
                   tasks {
-                    exec {
-                      commandLine = ['./gradlew', 'assemble', 'check']
-                    }
+                    exec { commandLine = ['./gradlew', 'assemble', 'check'] }
+                  }
+                }
+              }
+            }
+
+            stage("github-preview-release") {
+              environmentVariables = releaseCredentials(org)
+              jobs {
+                job("create-preview-release") {
+                  elasticProfileId = "ecs-gocd-dev-build"
+                  tasks {
+                    exec { commandLine = ['./gradlew', 'githubRelease'] }
+                  }
+                }
+              }
+            }
+
+            stage("github-release") {
+              approval { type = 'manual' }
+              environmentVariables = releaseCredentials(org) + ["PRERELEASE": "NO"]
+              jobs {
+                job("create-release") {
+                  elasticProfileId = "ecs-gocd-dev-build"
+                  tasks {
+                    exec { commandLine = ['./gradlew', 'githubRelease'] }
                   }
                 }
               }
